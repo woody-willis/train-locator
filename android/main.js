fa779dc9ae67a0aa82dbe3d84fa1e2e3340a2b6d
OffensivelyKind <woodyowillis@gmail.com>
2022-11-23 09:39:38 +0000

    Add front-back communication

--- android/main.js
+++ android/main.js
@@ -1,20 +1,6 @@
 const back = require('androidjs').back;
-const fs = require('fs');
-const path = require('path');
 
-
-// defin signal save-data to listen from front process
-
-back.on('save-data', function(filepath, msg){
-	fs.writeFile(path.join(filepath, 'data.txt'), msg, function(err){
-		if(err) throw err;
-		console.log('file saved')
-	})
-})
-
-back.on('get-data', function(filepath){
-	fs.readFile(path.join(filepath, 'data.txt'), 'utf-8', function(err, data){
-		if(err) back.send('get-data-result', '@@');
-		else back.send('get-data-result', data);
-	})
-})
+back.on('get-trains-from-stations', function (from, to) {
+	let html = "From: " + from + " | To: " + to;
+	back.send("trains-from-station-html", html)
+});
