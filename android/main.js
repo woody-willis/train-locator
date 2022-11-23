3df9b2ebc864c9c3831bb74e34ead1f0f50fa10c
OffensivelyKind <offensivelykind@gmail.com>
2022-11-23 21:28:38 +0000

    Update API

--- android/main.js
+++ android/main.js
@@ -1,6 +1,10 @@
 const back = require('androidjs').back;
+const axios = require('axios');
 
-back.on('get-trains-from-stations', function (from, to) {
-	let html = "From: " + from + " | To: " + to;
+const apiUrl = 'http://skywarspractice.ga:9090/v1/';
+
+back.on('get-trains-from-stations', async (from, to) => {
+	const response = await axios.get(apiUrl + 'get-trains-from-stations/' + from + '/' + to);
+	const html = JSON.parse(response.data).html;
 	back.send("trains-from-station-html", html)
 });
