ddac2125289cf67273814f4db39b55566a6a618d
OffensivelyKind <offensivelykind@gmail.com>
2022-11-23 22:05:21 +0000

    Fix typo

--- android/main.js
+++ android/main.js
@@ -4,7 +4,9 @@ const fetch = require('node-fetch');
 const apiUrl = 'http://skywarspractice.ga:9090/v1/';
 
 back.on('get-trains-from-stations', async (from, to) => {
-	const response = await fetch(apiUrl + 'get-trains-from-stations/' + from + '/' + to);
+	const response = await fetch(apiUrl + 'get-journey-html/' + from + '/' + to).catch(err => {
+		console.log(err);
+	});
 	const html = await response.text();
 	back.send("trains-from-station-html", html)
 });
