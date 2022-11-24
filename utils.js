7622beef177268bf294c49fab9d865cc1c9be9cb
OffensivelyKind <woodyowillis@gmail.com>
2022-11-24 20:04:17 +0000

    Test fix null service issue

--- utils.js
+++ utils.js
@@ -1,5 +1,8 @@
 module.exports.cleanServiceDetails = (serviceDetails) => {
     for (let i = 0; i < serviceDetails.length; i++) {
+        if (!serviceDetails[i]) {
+            continue;
+        }
         if (serviceDetails[i].at) {
             if (serviceDetails[i].at == "On time" || serviceDetails[i].at == "No report") {
                 serviceDetails[i].at = serviceDetails[i].st;
