b51ee800ede847502d4dc49297648faea491fc7d
OffensivelyKind <offensivelykind@gmail.com>
2022-11-24 21:19:00 +0000

    Set Content-Type to JSON

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const utils = require("./utils");
 // v1
 
 app.get("/v1/get-location-from-id/:id", async (req, res) => {
+    res.setHeader("Content-Type", "application/json");
     // Speed calculation
     const serviceID = decodeURI(req.params.id);
     let response = {};
@@ -278,6 +279,7 @@ app.get("/v1/get-location-from-id/:id", async (req, res) => {
 });
 
 app.get("/v1/get-journey-html/:from/:to", async (req, res) => {
+    res.setHeader("Content-Type", "application/json");
     const from = req.params.from;
     const to = req.params.to == "null" ? null : req.params.to;
     let html = "";
